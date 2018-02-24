f663ec6327def9ec50ca0be9397b664366cba1d4
MrAttoAttoAtto <atto.allas@gmail.com>
2018-02-24 23:38:29 +0000

    Is this perfect?

---
+++ src/main/java/com/bekos/genootics/Main.java
@@ -0,0 +1,43 @@
+/*
+ * Genootics Minecraft mod adding genetics to Minecraft
+ * Copyright (C) 2018  Robot Apocalypse Committee
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.bekos.genootics;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(name = Main.NAME, version = Main.VERSION, modid = Main.MODID)
+public class Main {
+    public static final String MODID = "genootics";
+    public static final String NAME = "Genootics";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+    }
+}
