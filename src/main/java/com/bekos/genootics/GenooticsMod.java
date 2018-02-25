68cdf5f50d8500164add576e89afd16093bde8f2
Joe <joe.bell34@btinternet.com>
2018-02-25 08:25:18 +0000

    Renamed

--- src/main/java/com/bekos/genootics/GenooticsMod.java
+++ src/main/java/com/bekos/genootics/GenooticsMod.java
@@ -23,8 +23,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(name = Main.NAME, version = Main.VERSION, modid = Main.MODID)
-public class Main {
+@Mod(name = GenooticsMod.NAME, version = GenooticsMod.VERSION, modid = GenooticsMod.MODID)
+public class GenooticsMod {
     public static final String MODID = "genootics";
     public static final String NAME = "Genootics";
     public static final String VERSION = "1.0";
