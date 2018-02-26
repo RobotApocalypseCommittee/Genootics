777a959e7f5fe3428d3e3304072e4d150af99cee
Atto Allas <atto.allas@gmail.com>
2018-02-26 22:47:46 +0000

    Just a bit of cleansing

--- src/main/java/com/bekos/genootics/block/BlockExtractor.java
+++ src/main/java/com/bekos/genootics/block/BlockExtractor.java
@@ -3,11 +3,10 @@ package com.bekos.genootics.block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 
-public class BlockExtractor  extends BlockBase {
+public class BlockExtractor extends BlockBase {
 
     public BlockExtractor() {
-        super(Material.ROCK, "machine_extractor");
-        setCreativeTab(CreativeTabs.REDSTONE);
+        super(Material.ROCK, "machineExtractor");
         setHardness(3f);
         setResistance(5f);
     }
