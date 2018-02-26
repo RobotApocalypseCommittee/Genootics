146341aa6c171f64373280550e1bcbcbb3f5c1f8
bhuvan21 <bhuvan.belur@westminster.org.uk>
2018-02-26 21:39:11 +0000

    Added the manual item - it is not openable but it exists now

--- src/main/java/com/bekos/genootics/items/ModItems.java
+++ src/main/java/com/bekos/genootics/items/ModItems.java
@@ -6,14 +6,18 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModItems {
 
     private static ItemSyringe itemSyringe = new ItemSyringe();
+    private static ItemManual itemManual = new ItemManual();
 
     public static void register(IForgeRegistry<Item> registry) {
         registry.registerAll(
-                itemSyringe
+                itemSyringe,
+                itemManual
         );
     }
 
     public static void registerModels() {
         itemSyringe.registerItemModel();
+        itemManual.registerItemModel();
     }
+
 }
