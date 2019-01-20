2a26d1d350c05f6012b07fbe69d6a43920582d41
Joe <joe.bell34@btinternet.com>
2019-01-20 18:19:00 +0000

    New Year and the Project Is Not Dead

--- src/main/java/com/bekos/genootics/machines/common/ContainerMachine.java
+++ src/main/java/com/bekos/genootics/machines/common/ContainerMachine.java
@@ -1,6 +1,6 @@
 /*
  * Genootics Minecraft mod adding genetics to Minecraft
- * Copyright (C) 2018  Robot Apocalypse Committee
+ * Copyright (C) 2019  Robot Apocalypse Committee
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
